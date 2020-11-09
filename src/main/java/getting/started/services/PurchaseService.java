fc626a86739755d9ca7e17006dd54bf0b226e3ba
JohnneSouza <jhonnesf5@gmail.com>
2020-11-09 01:54:46 -0300

    Optimizely Simple Discount Feature

--- src/main/java/getting/started/services/PurchaseService.java
+++ src/main/java/getting/started/services/PurchaseService.java
@@ -1,7 +1,10 @@
 package getting.started.services;
 
+import com.optimizely.ab.Optimizely;
+import com.optimizely.ab.OptimizelyFactory;
 import getting.started.models.PurchaseRequest;
 import getting.started.models.Result;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -12,23 +15,29 @@ import java.util.Set;
 @ApplicationScoped
 public class PurchaseService {
 
-    private String message = "You got a sell!";
-    private boolean isDiscountActive = true;
-    private float discount = 5.0f;
+
+    private static final String SDK_KEY = ConfigProvider.getConfig().getValue("optimizely.sdk.key", String.class);
+
+    Optimizely optimizelyClient = OptimizelyFactory.newDefaultInstance(SDK_KEY);
 
     @Inject
     Validator validator;
 
     public Result createPurchase(PurchaseRequest request) {
+        String message;
+        Boolean isDiscountActive = optimizelyClient.isFeatureEnabled("discount", request.getCustomer());
+        Integer discountAmount =
+                optimizelyClient.getFeatureVariableInteger("discount", "amount", request.getCustomer());
         Set<ConstraintViolation<PurchaseRequest>> violations = validatePurchaseRequest(request);
         if (violations.size() > 0){
             return new Result(violations);
         }
-        if (isDiscountActive){
-            message = String.format("Congratulations! You got a discount of %s%s!", discount, "%");
+        if (isDiscountActive) {
+            message = String.format("Congratulations! You got a discount of %s%s!", discountAmount, "%");
+        } else {
+            message = "You got a sell!";
         }
         return new Result(message);
-
     }
 
     private Set<ConstraintViolation<PurchaseRequest>> validatePurchaseRequest(PurchaseRequest request){
